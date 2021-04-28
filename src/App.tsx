f9924b8c2e885f2d7592e8157b38b8de362920d6
Mario-coding <laurenmario19@gmail.com>
2021-04-28 20:28:34 +0700

    message

--- src/App.tsx
+++ src/App.tsx
@@ -13,47 +13,12 @@ import {
   Dropdown,
 } from 'semantic-ui-react'
 
-const App = () => {
-  const [visible, setVisible] = React.useState(false)
-
-  return (
-    <Grid columns={1}>
-      <Grid.Column>
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar
-            as={Menu}
-            animation='overlay'
-            icon='labeled'
-            inverted
-            onHide={() => setVisible(false)}
-            vertical
-            visible={visible}
-            width='thin'
-          >
-            <Menu.Item as='a'>
-              <Icon name='home' />
-              Home
-            </Menu.Item>
-            <Menu.Item as='a'>
-              <Icon name='gamepad' />
-              Games
-            </Menu.Item>
-            <Menu.Item as='a'>
-              <Icon name='camera' />
-              Channels
-            </Menu.Item>
-          </Sidebar>
-
-          <Sidebar.Pusher dimmed={visible}>
-            <Segment basic>
-              <Header as='h3'>Application Content</Header>
-              <Image src='/images/wireframe/paragraph.png' />
-            </Segment>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </Grid.Column>
-    </Grid>
-  )
+function App () {
+  return(
+    <div className='App'>
+      <h1>Hello!</h1>
+    </div>
+  );
 }
 
 export default App;
